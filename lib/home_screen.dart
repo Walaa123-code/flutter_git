0e3a42437c2dcf84e51228154f90e8cc2903de8f
Walaa Elsawi <walaatealab@gmail.com>
2024-12-10 09:20:16 +0200

    Changed background color in appBar in scaffold.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,7 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: Text('Git'),
       ),
     );
